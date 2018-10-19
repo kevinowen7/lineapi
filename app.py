88e4a6bfff1d29a624e9efa92aef4613fb5391bd
snorlak1999 <39644873+snorlak1999@users.noreply.github.com>
2018-10-19 23:35:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -70,6 +70,8 @@ def webhook():
 
 def makeWebhookResult(req):  
     a = req.get("id")
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
     return {
             "speech": "w",
             "displayText": "w",
@@ -77,9 +79,6 @@ def makeWebhookResult(req):
             #"contextOut": [],
             "source": "w"
         }
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
