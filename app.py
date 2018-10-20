3a34d4447c6ad85ee13b11019ddcf5c380f79554
snorlak1999 <39644873+snorlak1999@users.noreply.github.com>
2018-10-20 11:42:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,61 +40,32 @@ firebase_admin.initialize_app(cred,{
 # Flask app should start in global layout
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('fHIvKaGYZfXuMVXMt/tG1WpFztLQYqzlhDp5DB/Memvyo7TfrObM2bpXTS/W1jwlWsQulRJylBl3seFXcWr10Zu2SJldz8Qxd5sdBxxEQa3uXvxKNhU84K8CLYQ4yFstk2dhUdjEkdkMyzbzfgUHwAdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('tRo0KibnDeYJgRVUj01Nnh0+MSCTUhbyZo0HgSwtfRZzGt5Gh0kZUUuiDJkOswWWWsQulRJylBl3seFXcWr10Zu2SJldz8Qxd5sdBxxEQa2k374wJdd1vcNQVrGOusGnFErAt4SPvq4FhZLUdN1vEgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('0d184e88d0b01d9a5586b06abd6a1250')
 
 @app.route('/webhook', methods=['POST'])
 
 
 def webhook():
-    req = request.get_json(silent=True, force=True)
-    res = makeWebhookResult(req)  
-    
-    res = json.dumps(res, indent=4)
-    print(res)
-    res2 = {
-            "speech": "tgl2",
-            "displayText": "tgl2",
-            #"data": {},
-            #"contextOut": [],
-            "source": "tgl2"
-        }
-    
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    
-    return r
-
-
-
-
-def makeWebhookResult(req):  
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    return {
-            "speech": "w",
-            "displayText": "w",
-            #"data": {},
-            #"contextOut": [],
-            "source": "w"
-        }
+    
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
     # handle webhook body
     w = handler.handle(body, signature)
-    return {
-            "speech": w,
-            "displayText": w,
-            #"data": {},
-            #"contextOut": [],
-            "source": w
-        }
+    return 'ok'
+
+
+
         
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    return event.resource.userId
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
     
     
 if __name__ == '__main__':
